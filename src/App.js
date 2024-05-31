7cd1f10a9f79e9ab09b219cc9e4b652107db5a67
Matt Hatoun <matt.hatoun@gmail.com>
2024-05-31 15:40:23 -0400

    show items in list

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
+import React, { useState, useRef } from 'react';
 import './App.css';
 import Header from './Header';
 import Footer from './Footer';
 import NewTodo from './NewTodo';
 import List from './List';
 
+const initialData = {
+  todo: ["Buy groceries", "Walk the dog", "Do laundry", "Wash dishes"],
+  doing: [],
+  done: [],
+};
+
 function App() {
+  const [listItems, setListItems] = useState(initialData);
   return (
     <div id="app">
       <Header />
       <div id="content">
         <NewTodo />
         <div id="lists" className="display-flex flex-wrap">
-          <List name="Todo" />
+          <List name="Todo" items={listItems['todo']} />
           <List name="Doing" />
           <List name="Done" />
         </div>
--- src/List.js
+++ src/List.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
-const List = ({name}) => {
+const List = ({name, items}) => {
   return (
     <div className='list flex-grow-1 rounded-corners border-mild'>
       <div className='list-title font-pop'>
         {name}
       </div>
+      {items && items.map((item, index) => {
+        return (
+          <div key={index} className='list-item'>
+            {item}
+          </div>
+        );
+      })}
     </div>
   );
 };
