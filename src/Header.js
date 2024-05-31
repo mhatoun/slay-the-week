c961fdf278b525922fe41cb4e25855179386d39e
Matt Hatoun <matt.hatoun@gmail.com>
2024-05-31 14:30:24 -0400

    cleanup

--- src/Header.js
+++ src/Header.js
@@ -3,13 +3,13 @@ import React from 'react';
 const Header = () => {
   return (
     <header id="header" className='display-flex flex-between'>
-      <div id='logo' className='display-flex'>
+      <div className='display-flex'>
         <div id='logo-slay' className='font-pop'>SLAY</div>
         <div id='logo-week' className='font-chunky'>the Week</div>
       </div>
       <div className='pointer'>
         Account
-        &#9660;
+        <small> &#9660; </small>
       </div>
     </header>
   );
