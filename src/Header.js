678d11e47cce82767b29cf617135e1a5ca4e641c
Matt Hatoun <matt.hatoun@gmail.com>
2024-05-31 14:23:45 -0400

    Account link

--- src/Header.js
+++ src/Header.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header id="header">
+    <header id="header" className='display-flex flex-between'>
       <div id='logo' className='display-flex'>
         <div id='logo-slay' className='font-pop'>SLAY</div>
         <div id='logo-week' className='font-chunky'>the Week</div>
+      </div>
+      <div className='pointer'>
+        Account
+        &#9660;
       </div>
     </header>
   );
