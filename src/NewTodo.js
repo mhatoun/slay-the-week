4db6a4993132224bfd13069bd0f49de922efb1e6
Matt Hatoun <matt.hatoun@gmail.com>
2024-05-31 15:40:37 -0400

    basic layout for new form

--- src/NewTodo.js
+++ src/NewTodo.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
-const NewTodo = () => {
+const NewTodo = ({ newTask, setNewTask, handleAddTask }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleAddTask();
+  };
+
   return (
-    <div>
-      
+    <div id='new-todo'>
+      <form onSubmit={handleSubmit}>
+        <div className='input-button-wrapper display-flex flex-between'>
+          <input className='flex-grow-1 height-100'
+            type="text"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+            placeholder="Add a new task"
+          />
+          <button className='primary-button height-100' type="submit">Add</button>
+        </div>
+      </form>
     </div>
   );
 };
