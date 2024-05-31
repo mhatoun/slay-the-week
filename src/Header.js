2fef84cb9e253998cfc765c1c51c0ebf46174827
Matt Hatoun <matt.hatoun@gmail.com>
2024-05-31 14:10:09 -0400

    header and logo

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Header = () => {
   return (
     <header id="header">
-      SLAY the Week
+      <div id='logo' className='display-flex'>
+        <div id='logo-slay' className='font-pop'>SLAY</div>
+        <div id='logo-week' className='font-chunky'>the Week</div>
+      </div>
     </header>
   );
 };
