5dd88cc2c4a92669974fa9d90083de2935d1b130
Matt Hatoun <matt.hatoun@gmail.com>
2024-05-31 22:19:15 -0400

    temp working version of ellipsis for long items

--- src/List.js
+++ src/List.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const List = ({name, listKey, items, handleDragStart, handleDrop, handleDragOver}) => {
   return (
-    <div className='list flex-grow-1 rounded-corners border-contrast'>
+    <div className='list flex-grow-1 rounded-corners border-contrast flex-item'>
       <div className='list-title font-pop'>
         {name}
       </div>
       {items && items.map((item, index) => {
         return (
           <div 
-            key={index} className='list-item grab'
+            key={index} className='list-item grab flex-item'
             draggable='true'
             onDragStart={(e) => handleDragStart(e, index, listKey)}
             onDrop={(e) => handleDrop(e, index, listKey)}
