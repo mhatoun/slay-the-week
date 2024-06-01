29f175745e9810a86e017809ed3be054e0ce8aad
Matt Hatoun <matt.hatoun@gmail.com>
2024-06-01 07:46:15 -0400

    cleanup limited with css

--- src/List.js
+++ src/List.js
@@ -2,14 +2,18 @@ import React from 'react';
 
 const List = ({name, listKey, items, handleDragStart, handleDrop, handleDragOver}) => {
   return (
-    <div className='list flex-grow-1 rounded-corners border-contrast flex-item'>
-      <div className='list-title font-pop'>
+    <div 
+      className='list flex-grow-1 rounded-corners border-contrast' >
+      <div 
+        className='list-title font-pop' 
+        onDrop={(e) => handleDrop(e, 0, listKey)}
+        onDragOver={(e) => handleDragOver(e)}>
         {name}
       </div>
       {items && items.map((item, index) => {
         return (
           <div 
-            key={index} className='list-item grab flex-item'
+            key={index} className='list-item grab'
             draggable='true'
             onDragStart={(e) => handleDragStart(e, index, listKey)}
             onDrop={(e) => handleDrop(e, index, listKey)}
